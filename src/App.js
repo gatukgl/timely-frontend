00bc86a32a198bead90303c93ca2885a5dd736f5
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-03 20:53:28 +0700

    Add remove button and adjust dropdown

--- src/App.js
+++ src/App.js
@@ -3,15 +3,15 @@ import React from 'react';
 function App() {
   return (
     <div className='App'>
-      <nav class='navbar navbar-expand-lg fixed-top navbar-dark bg-dark'>
-        <a class='navbar-brand mr-auto mr-lg-0' href='.'>
+      <nav className='navbar navbar-expand-lg fixed-top navbar-dark bg-dark'>
+        <a className='navbar-brand mr-auto mr-lg-0' href='.'>
           Timely
         </a>
       </nav>
 
-      <div class='container' role='main' style={{ marginTop: '100px' }}>
-        <div class='row'>
-          <div class='col-8'>
+      <div className='container' role='main' style={{ marginTop: '100px' }}>
+        <div className='row'>
+          <div className='col-8'>
             <input
               type='text'
               placeholder='What are you working on?'
@@ -21,35 +21,35 @@ function App() {
 
           <div className='dropdown'>
             <button
-              class='btn btn-lg btn-secondary dropdown-toggle'
+              className='btn btn-lg btn-secondary dropdown-toggle'
               type='button'
-              id='dropdownMenuButton'
+              id='categoryDropdown'
               data-toggle='dropdown'
               aria-haspopup='true'
-              aria-expanded='false'
+              aria-expanded='true'
             >
               Category
             </button>
-            <div class='dropdown-menu' aria-labelledby='dropdownMenuButton'>
-              <a class='dropdown-item' href='#'>
+            <div className='dropdown-menu' aria-labelledby='categoryDropdown'>
+              <a className='dropdown-item' href='#'>
                 Action
               </a>
-              <a class='dropdown-item' href='#'>
+              <a className='dropdown-item' href='#'>
                 Another action
               </a>
-              <a class='dropdown-item' href='#'>
+              <a className='dropdown-item' href='#'>
                 Something else here
               </a>
             </div>
           </div>
 
           <div className='col'>
-            <button type='button' class='btn btn-success btn-lg'>
+            <button type='button' className='btn btn-success btn-lg'>
               Start
             </button>
           </div>
 
-          <div className='col' style={{ fontSize: '1.9em'}}>
+          <div className='col' style={{ fontSize: '1.9em' }}>
             00:00:00
           </div>
         </div>
@@ -58,17 +58,71 @@ function App() {
             <thead scope='col'>
               <tr>
                 <th>Today</th>
+                <th></th>
+                <th></th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>Learn about React</td>
+                <td>9:00 - 10:00</td>
+                <td>
+                  <button className='btn btn-danger'>Remove</button>
+                </td>
               </tr>
               <tr>
-                <td>Learn about React</td>
+                <td>
+                  <span>Learn about JSX</span>
+                  <span class='badge badge-info ml-2'>study</span>
+                </td>
+                <td>10:05 - 12:00</td>
+                <td>
+                  <button className='btn btn-danger'>Remove</button>
+                </td>
               </tr>
               <tr>
-                <td>Learn about React</td>
+                <td>
+                  <span>Learn about virtual DOM</span>
+                  <span class='badge badge-info ml-2'>study</span>
+                </td>
+                <td>13:35 - 14:00</td>
+                <td>
+                  <button className='btn btn-danger'>Remove</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className='row'>
+          <table className='table table-hover'>
+            <thead scope='col'>
+              <tr>
+                <th>Yesterday</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <span>Learn about JavaScript</span>
+                  <span class='badge badge-info ml-2'>study</span>
+                </td>
+                <td>10:35 - 12:00</td>
+                <td>
+                  <button className='btn btn-danger'>Remove</button>
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <span>Dance with friends</span>
+                  <span class='badge badge-warning ml-2'>Workout</span>
+                </td>
+                <td>13:35 - 14:00</td>
+                <td>
+                  <button className='btn btn-danger'>Remove</button>
+                </td>
               </tr>
             </tbody>
           </table>
