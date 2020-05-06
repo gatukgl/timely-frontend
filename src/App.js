16f76280f15d03f95b94b8c6f2d0641f68ffb0a9
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-06 10:52:48 +0700

    Get all tasks from api

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React from 'react'
+import axios from 'axios'
 
 class App extends React.Component {
+  state = {
+    allTasks: [],
+  }
+
+  componentDidMount() {
+    axios.get('localhost:8000/tasks').then(function (response) {
+      console.log(response.json())
+      this.setState({ allTasks: response.json() })
+    })
+  }
+
   render() {
     return (
       <div className='App'>
@@ -55,72 +67,45 @@ class App extends React.Component {
             </div>
           </div>
 
-          <div className='my-3 bg-white rounded shadow-sm'>
-            <h6 className='border-bottom border-gray p-3 mb-4'>Today</h6>
-            <div className='row m-2 py-2 border-bottom border-gray align-items-center d-flex justify-content-between'>
-              <div className='col'>
-                <span>Learn about React</span>
-                <span className='ml-2 badge badge-info'>Study</span>
+          {this.state.allTasks ? (
+            <div class='text-center p-5 my-3 rounded shadow-sm text-dark'>
+              <span>No task yet</span>
+            </div>
+          ) : (
+            <div className='my-3 bg-white rounded shadow-sm'>
+              <h6 className='border-bottom border-gray p-3 mb-4'>Today</h6>
+              <div className='row m-2 py-2 border-bottom border-gray align-items-center d-flex justify-content-between'>
+                <div className='col'>
+                  <span>Learn about React</span>
+                  <span className='ml-2 badge badge-info'>Study</span>
+                </div>
+                <div className='col'>9:00:00 - 10:00:23</div>
+                <div className=''>
+                  <button class='btn btn-danger'>Remove</button>
+                </div>
               </div>
-              <div className='col'>9:00:00 - 10:00:23</div>
-              <div className=''>
-                <button class='btn btn-danger'>Remove</button>
+              <div className='row m-2 py-2 border-bottom border-gray align-items-center d-flex justify-content-between'>
+                <div className='col'>
+                  <span>Go to gym</span>
+                  <span className='ml-2 badge badge-warning'>Workout</span>
+                </div>
+                <div className='col'>9:00:00 - 10:00:23</div>
+                <div className=''>
+                  <button class='btn btn-danger'>Remove</button>
+                </div>
+              </div>
+              <div className='row m-2 py-2 align-items-center d-flex justify-content-between'>
+                <div className='col'>
+                  <span>Learn how to cook</span>
+                  <span className='ml-2 badge badge-info'>Study</span>
+                </div>
+                <div className='col'>9:00:00 - 10:00:23</div>
+                <div className=''>
+                  <button class='btn btn-danger'>Remove</button>
+                </div>
               </div>
             </div>
-            <div className='row m-2 py-2 border-bottom border-gray align-items-center d-flex justify-content-between'>
-              <div className='col'>
-                <span>Go to gym</span>
-                <span className='ml-2 badge badge-warning'>Workout</span>
-              </div>
-              <div className='col'>9:00:00 - 10:00:23</div>
-              <div className=''>
-                <button class='btn btn-danger'>Remove</button>
-              </div>
-            </div>
-            <div className='row m-2 py-2 align-items-center d-flex justify-content-between'>
-              <div className='col'>
-                <span>Learn how to cook</span>
-                <span className='ml-2 badge badge-info'>Study</span>
-              </div>
-              <div className='col'>9:00:00 - 10:00:23</div>
-              <div className=''>
-                <button class='btn btn-danger'>Remove</button>
-              </div>
-            </div>
-          </div>
-
-          <div className='my-3 bg-white rounded shadow-sm'>
-            <h6 className='border-bottom border-gray p-3 mb-4'>Yesterday</h6>
-            <div className='row m-2 py-2 border-bottom border-gray align-items-center d-flex justify-content-between'>
-              <div className='col'>
-                <span>Learn about React</span>
-                <span className='ml-2 badge badge-info'>Study</span>
-              </div>
-              <div className='col'>9:00:00 - 10:00:23</div>
-              <div className=''>
-                <button class='btn btn-danger'>Remove</button>
-              </div>
-            </div>
-            <div className='row m-2 py-2 border-bottom border-gray align-items-center d-flex justify-content-between'>
-              <div className='col'>
-                <span>Go to gym</span>
-                <span className='ml-2 badge badge-warning'>Workout</span>
-              </div>
-              <div className='col'>9:00:00 - 10:00:23</div>
-              <div className=''>
-                <button class='btn btn-danger'>Remove</button>
-              </div>
-            </div>
-            <div className='row m-2 py-2 align-items-center d-flex justify-content-between'>
-              <div className='col'>
-                <span>Learn how to cook</span>
-              </div>
-              <div className='col'>9:00:00 - 10:00:23</div>
-              <div className=''>
-                <button class='btn btn-danger'>Remove</button>
-              </div>
-            </div>
-          </div>
+          )}
         </div>
       </div>
     )
