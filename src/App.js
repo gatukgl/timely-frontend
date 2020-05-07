2a418f1620af709d617a94118e4086f32ac22157
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-07 21:44:55 +0700

    Change to use hook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { DateTime } from 'luxon'
 import * as R from 'ramda'
@@ -14,97 +14,93 @@ import { getTasks, createTask, removeTask } from './apis'
 import { restructureTask } from './utils'
 
 const BASE_URL = 'http://localhost:8000'
-class App extends React.Component {
-  state = {
-    allTasks: [],
-    task: '',
-    category: '',
-    isWatchStarted: false,
-    startAt: '',
-    endAt: ''
-  }
+const App = () => {
+  const [allTasks, setAllTasks] = useState([])
+  const [task, setTask] = useState('')
+  const [selectedCategory, setSelectedCategory] = useState('')
+  const [isWatchStarted, setWatchStarted] = useState(false)
+  const [startedAt, setStartedAt] = useState('')
+  const [endedAt, setEndedAt] = useState('')
 
-  componentDidMount() {
-    const response = getTasks().then((response) => {
-      this.setState({ allTasks: response.map((task) => restructureTask(task)) })
+  useEffect(() => {
+    getTasks().then((response) => {
+      const restructuredTasks = response.map((task) => restructureTask(task))
+      setAllTasks(restructuredTasks)
     })
-  }
+  }, [])
 
-  removeTask = (taskId) => () => {
+  const removeTaskAndSetState = (taskId) => () => {
     removeTask(taskId).then(() => {
       const isRemovedTasks = (task) => task.id === taskId
-      const allTasks = R.reject(isRemovedTasks, this.state.allTasks)
-      this.setState({ allTasks })
+      const tasks = R.reject(isRemovedTasks, allTasks)
+      setAllTasks(tasks)
     })
   }
 
-  onTextChange = (event) => {
-    const {
-      target: { value, name }
-    } = event
-    this.setState({ [name]: value })
+  const onTaskChange = (event) => {
+    setTask(event.target.value)
   }
 
-  onDropdownChange = (event) => {
-    this.setState({ category: event.target.value })
+  const onCategoryChange = (event) => {
+    setTask(event.target.value)
   }
 
-  onStartWatch = (isStart) => () => {
-    this.setState({ isWatchStarted: isStart })
+  const onDropdownChange = (event) => {
+    setSelectedCategory(event.target.value)
+  }
+
+  const onStartWatch = (isStart) => () => {
+    setWatchStarted(isStart)
     const currentDateTime = DateTime.local().toUTC()
 
     if (isStart) {
-      this.setState({ startAt: currentDateTime })
+      setStartedAt(currentDateTime)
     } else {
-      this.setState({ endAt: currentDateTime })
-      const task = {
-        name: this.state.task,
-        category: this.state.category,
-        started_at: this.state.startAt,
+      setEndedAt(currentDateTime)
+      const taskItem = {
+        name: task,
+        category: selectedCategory,
+        started_at: startedAt,
         ended_at: currentDateTime
       }
-      this.createTask(task)
+      createTaskAndSetState(taskItem)
     }
   }
 
-  createTask = (task) => {
+  const createTaskAndSetState = (task) => {
     createTask(task).then((response) => {
       const newTask = restructureTask(response)
-      const allTasks = this.state.allTasks.concat(newTask)
-      this.setState({
-        allTasks
-      })
+      const tasks = allTasks.concat(newTask)
+      setAllTasks(tasks)
     })
   }
 
-  render() {
-    return (
-      <div className='App'>
-        <NavBar />
-        <div className='container' role='main' style={{ marginTop: '100px' }}>
-          <div className='row'>
-            <TaskInput state={this.state} onChange={this.onTextChange} />
-            <Categories selectedCategory={this.state.category} onChange={this.onDropdownChange} />
-            <WatchToggledButton isStarted={this.state.isWatchStarted} onStart={this.onStartWatch} />
-            <StopWatch isEnabled={this.state.isWatchStarted} />
-          </div>
-
-          {this.state.allTasks.length < 1 ? (
-            <div className='text-center p-5 my-3 rounded shadow-sm text-dark'>
-              <span>No task yet</span>
-            </div>
-          ) : (
-            <div className='my-3 bg-white rounded shadow-sm'>
-              <h6 className='border-bottom border-gray p-3 mb-4'>Today</h6>
-              {this.state.allTasks.map((task) => (
-                <Task task={task} removeTask={this.removeTask(task.id)} />
-              ))}
-            </div>
-          )}
+  return (
+    <div className='App'>
+      <NavBar />
+      <div className='container' role='main' style={{ marginTop: '100px' }}>
+        <div className='row'>
+          <TaskInput state={task} onChange={onTaskChange} />
+          <Categories selectedCategory={selectedCategory} onChange={onDropdownChange} />
+          <WatchToggledButton isStarted={isWatchStarted} onStart={onStartWatch} />
+          <StopWatch isEnabled={isWatchStarted} />
         </div>
+
+        {allTasks.length < 1 ? (
+          <div className='text-center p-5 my-3 rounded shadow-sm text-dark'>
+            <span>No task yet</span>
+          </div>
+        ) : (
+          <div className='my-3 bg-white rounded shadow-sm'>
+            <h6 className='border-bottom border-gray p-3 mb-4'>Today</h6>
+            {allTasks.map((task) => (
+              <Task key={task.id} task={task} removeTask={removeTaskAndSetState(task.id)} />
+            ))}
+          </div>
+        )}
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default App
