8a90f72f4411cf07481590751cdb3f659c145cba
Gatuk Chattanon <gatukgl@gmail.com>
2020-05-07 22:37:43 +0700

    Fix stopwatch does not activate

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Stopwatch from 'react-stopwatch'
 
-export const StopWatch = ({ isEnabled }) => (
-  <div className='col' style={{ fontSize: '1.9em' }}>
-    {isEnabled ? (
-      <Stopwatch
-        autoStart
-        seconds={0}
-        minutes={0}
-        hours={0}
-        render={({ formatted }) => <p>{formatted}</p>}
-      />
-    ) : (
-      '00:00:00'
-    )}
-  </div>
-)
+import { TaskContext } from './TaskContext'
+
+export const StopWatch = () => {
+  const { isWatchStarted } = useContext(TaskContext)
+
+  return (
+    <div className='col' style={{ fontSize: '1.9em' }}>
+      {isWatchStarted ? (
+        <Stopwatch
+          autoStart
+          seconds={0}
+          minutes={0}
+          hours={0}
+          render={({ formatted }) => <p>{formatted}</p>}
+        />
+      ) : (
+        '00:00:00'
+      )}
+    </div>
+  )
+}
