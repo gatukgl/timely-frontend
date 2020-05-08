3bc2b295e7f1ddc30642b98141ec3ec192bd4d51
zkan <zkan.cs@gmail.com>
2020-05-08 22:07:02 +0700

    :sparkles: Show current date :bear:

--- src/Task.js
+++ src/Task.js
@@ -1,5 +1,12 @@
 import React from 'react'
 
+const getCurrentDate = () => {
+  let current_datetime = new Date()
+  let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate()
+
+  return formatted_date
+}
+
 export const Task = ({ task, removeTask }) => (
   <div key={task.id}>
     <div className='row m-2 py-2 border-bottom border-gray align-items-center d-flex justify-content-between'>
@@ -7,6 +14,7 @@ export const Task = ({ task, removeTask }) => (
         <span>{task.name}</span>
         <span className='ml-2 badge badge-info'>{task.category}</span>
       </div>
+      <div className='col'>{`${getCurrentDate()}`}</div>
       <div className='col'>{`${task.startTime} - ${task.endTime}`}</div>
       <div className=''>
         <button className='btn btn-danger' onClick={removeTask}>
