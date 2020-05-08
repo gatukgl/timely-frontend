3c7497b1bcd3812c7914fffea384af6920a10c9b
zkan <zkan.cs@gmail.com>
2020-05-08 22:50:22 +0700

    :recycle: Use startDate from restructureTask func :bear:

--- src/Task.js
+++ src/Task.js
@@ -1,12 +1,5 @@
 import React from 'react'
 
-const getCurrentDate = () => {
-  let currentDatetime = new Date()
-  let formattedDate = currentDatetime.getFullYear() + "-" + (currentDatetime.getMonth() + 1) + "-" + currentDatetime.getDate()
-
-  return formattedDate
-}
-
 export const Task = ({ task, removeTask }) => (
   <div key={task.id}>
     <div className='row m-2 py-2 border-bottom border-gray align-items-center d-flex justify-content-between'>
@@ -15,7 +8,7 @@ export const Task = ({ task, removeTask }) => (
         <span className='ml-2 badge badge-info'>{task.category}</span>
       </div>
       <div className='col'>{`${task.username}`}</div>
-      <div className='col'>{`${getCurrentDate()}`}</div>
+      <div className='col'>{`${task.startDate}`}</div>
       <div className='col'>{`${task.startTime} - ${task.endTime}`}</div>
       <div className=''>
         <button className='btn btn-danger' onClick={removeTask}>
